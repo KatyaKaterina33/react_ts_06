6361b371dd25be09123be1c83a1b8323224c694b
EkaterinaSokolova <sokolovaekaterina.v@gmail.com>
2023-03-14 12:31:52 +0100

    Second homework_Counter

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react';
+import React from 'react';
 
-function App() {
+import {Button} from './components/Button.js';
+import {Display} from './components/Display.js';
+
+const App = () => {
+  const [counter, setCounter] = useState(0);
+  console.log('rendering with counter value = ', counter);
+  const increaseByOne = () => setCounter(counter + 1);
+  const decreaseByOne = () => setCounter(counter - 1);
+  const setToZero = () => setCounter(0);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Display counter = {counter} />
+      <Button handleClick = {increaseByOne} text = 'plus' />
+      <Button handleClick = {decreaseByOne} text = 'minus' />
+      <Button handleClick = {setToZero} text = 'zero' />
     </div>
   );
 }
