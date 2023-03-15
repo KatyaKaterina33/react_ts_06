68f28ad0200b5bf6c5a0a6e9f8c1e6ff630c6a1e
EkaterinaSokolova <sokolovaekaterina.v@gmail.com>
2023-03-15 14:05:32 +0100

    App1 first task, App second task

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+const Header = (props) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <h1>{props.title}</h1>
   );
+}
+
+const Content = (props) => {
+  return (
+      <p>
+        Part {props.num}: {props.part}. Number od exercises are {props.exercises}
+      </p>
+  );
+}
+
+const Total = (props) => {
+  return (
+      <p>Total number of exercises are {props.total}</p>
+  );
+}
+
+const App = () => {
+  const course = "Half Stack application development",
+        part1 = "Fundamentals of React",
+        exercises1 = 10,
+        part2 = "Using props to pass data",
+        exercises2 = 7,
+        part3 = "State of a component",
+        exercises3 = 14;
+return (
+  <div>
+    <Header title={course} />
+    <Content num={1} part={part1} exercises={exercises1} />
+    <Content num={2} part={part2} exercises={exercises2} />
+    <Content num={3} part={part3} exercises={exercises3} />
+    <Total total={exercises1+exercises2+exercises3} />
+  </div>
+);
 }
 
 export default App;
